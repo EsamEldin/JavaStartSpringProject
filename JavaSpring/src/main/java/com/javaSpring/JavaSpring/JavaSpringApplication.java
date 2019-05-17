05117fa2c64ac2c338ea3b6de333f4ea3f0f3fb5
EssamEldin <esam.eldin.ahmed@gmail.com>
2019-05-17 20:18:30 +0300

    test2

--- JavaSpring/src/main/java/com/javaSpring/JavaSpring/JavaSpringApplication.java
+++ JavaSpring/src/main/java/com/javaSpring/JavaSpring/JavaSpringApplication.java
@@ -8,7 +8,7 @@ public class JavaSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringApplication.class, args);
-		System.out.println("hhhhhh");
+		System.out.println("essam");
 	}
 
 }
